4848d2aaaec427ea07680aa7e042f5365afbf465
RifanMuhammed <rifanmuhammed2028@cs.sjcetpalai.ac.in>
2024-10-01 14:51:18 +0530

    Add files via upload

---
+++ studentdetails.py
@@ -0,0 +1,7 @@
+name=input("enter the name of the student:")
+roll_number=int(input("enter roll number"))
+roll_number=roll_number+1
+cgpa=float(input("enter cgpa"))
+percentage=cgpa*10
+print("cgpa percentage:",percentage,'%')
+print(name,roll_number)
