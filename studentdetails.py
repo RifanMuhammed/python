44807c080e538ff1790564144bbd37f28e782926
RifanMuhammed <rifanmuhammed2028@cs.sjcetpalai.ac.in>
2024-10-01 15:19:55 +0530

    Add files via upload
    document header added

--- studentdetails.py
+++ studentdetails.py
@@ -1,3 +1,6 @@
+'''Author:Rifan Muhammed
+   dATE:1-10-2024
+   Python program to print roll no and cgpa'''
 name=input("enter the name of the student:")
 roll_number=int(input("enter roll number"))
 roll_number=roll_number+1
