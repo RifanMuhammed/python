f2036655c3ddd07f671227172a3e58c08ddaabab
RifanMuhammed <rifanmuhammed2028@cs.sjcetpalai.ac.in>
2024-10-05 16:17:19 +0530

    pythontaskfile1.py

--- pythontaskfile1.py
+++ pythontaskfile1.py
@@ -1,6 +1,6 @@
 '''AUTHOR:RIFAN MUHAMMED
     DATE:5-10-24
     python program to print name and age of a person'''
-name=input("enter name of student:")
-age=int(input('enter age of the student:'))
+name=input("enter name of person:")
+age=int(input('enter age of the person:'))
 print('hello',name,'you are',age,'years old')
