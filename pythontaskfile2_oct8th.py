ba09b9ccc36ff6adeff89d648447a0d9cf806161
RifanMuhammed <rifanmuhammed2028@cs.sjcetpalai.ac.in>
2024-10-08 14:56:38 +0530

    Delete pythontaskfile2_oct8th.py

--- pythontaskfile2_oct8th.py
+++
@@ -1,9 +0,0 @@
-#Title: Create, concatenate, and print a string and access a sub-string from a given string.
-
-FIRST_NAME="RIFAN"
-LAST_NAME="MUHAMMED"
-NEW_STRING=(FIRST_NAME+" "+LAST_NAME)
-print("FULL NAME:",NEW_STRING)
-EXTRACTED_LASTNAME=(NEW_STRING[6:14])
-print("EXTRACTED LAST NAME:",EXTRACTED_LASTNAME)
-
